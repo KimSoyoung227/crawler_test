2608763c52c4b3ccf064f7a83910448a8da5bebf
soyoung.kim@konantech.com <soyoung.kim@konantech.com>
2020-02-27 15:06:01 +0900

    크롤링 소스 완성본

--- test2.py
+++ test2.py
@@ -6,41 +6,50 @@ import pandas as pd
 import numpy as np
 import os
 
-base_dir="D:\99.project\08.BMT\crawling"
-file_name = "sport.xlsx"
+base_url = "https://www.koreatimes.co.kr"   # 기본 사이트 url
+base_dir="D:\99.project\08.BMT\crawling"    # 생성될 엑셀파일 경로
+file_name = "sports.xlsx"                   # 생성될 엑셀파일명
 xlxs_dir = os.path.join(base_dir, file_name)
 result_title = []
 result_content = []
-content_url = []
+contents_url = []
 
-base_url = "https://www.koreatimes.co.kr/www/section_600_{}.html"
-#페이지로 이동하기
-for n in range(1,100,+1):
+
+# 페이지로 이동하기
+for n in range(1,50,+1):
+    print(n);
     if n == 1:
-        url = "https://www.koreatimes.co.kr/www/section_600.html"
+        url = base_url + "/www/sublist_600.html"
     else:
-        url = base_url.format(n) 
+        url = base_url + "/www/sublist_600_{}.html".format(n)
     html = urlopen(url)
     source = html.read()
     html.close()
     
-    #제목과 url 가져오기, 제목 배열에 append하기
+    # 제목과 url 가져오기, 제목 배열에 append하기
     soup = BeautifulSoup(source, "html.parser")
     title_container = soup.find("div", {"class":"all_section"})
-    #print(title_container)
-    titles = title_container.find_all("div",{"class":"list2_article_headline HD"})
-    #print(titles)
-
+    titles = title_container.find_all("div",{"class":"list_article_headline HD"})
+    # print(titles)
+    
     for title in titles:
         result_title.append(title.get_text())
-        for curl in title.find_all('a'):
-            print(curl['href'])
-            content_url.append(curl['href'])
-
-        print(content_url)
-    print(result_title)
         
-        
-#가져온 url로 이동하여 내용 가져오기, 내용 배열에 append하기
+        # 가져온 url로 이동하여 내용 가져오기, 내용 배열에 append하기
+        content_url = base_url + title.find('a')['href']
+        html2 = urlopen(content_url)
+        source2 = html2.read()
+        html2.close()
 
-##제목 내용을 엑셀에 넣기
+        soup2 = BeautifulSoup(source2, "html.parser")
+        content_container = soup2.find("div", {"id":"startts"})
+        result_content.append(content_container.text)
+        # print(len(content_container))
+    
+# 제목,내용을 엑셀에 넣기
+data = {"제목" : result_title, "내용" : result_content}
+df = pd.DataFrame(data, columns=['제목','내용'])
+writer = pd.ExcelWriter(file_name, engine='xlsxwriter')
+df.to_excel(writer,sheet_name='Sheet1')
+
+writer.close()
